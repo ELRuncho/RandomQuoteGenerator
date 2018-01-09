a7d4f8cf662867c1596a3225682ade0d95f97e4c
Rafael Franco <francorafae@gmail.com>
2018-01-09 18:22:43 -0500

    testing

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
-$("#Clicky").on("click", function(){
-	$.getJSON("http://quotes.rest/quote/random.json",function(json){
-		$(".quote").html(JSON.stringify(json));
+$(function(){	
+	$("#Clicky").on("click", function(){
+		$.getJSON("http://quotes.rest/qod.json";function(json){
+			var q1 = "";
+			json.forEach(function(val){
+				var keys = Object.keys(val);
+				q1 += "<div class = 'quote2'>";
+				keys.forEach(function(key){
+					q1+="<strong>"+ key + "</strong>: "+ val[key]+"<br>";
+				});
+				q1+="</div><br>";
+			});
+			$(".quote").html(q1);
+		});
 	});
-})
+});
